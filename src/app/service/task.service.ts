8323f8cdff55d64e9de6a9dbe2bdf014fe44121a
walter.liendo@gmail.com <walter.liendo@gmail.com>
2022-04-22 22:17:55 -0300

    binding2

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TASKS } from '../mock-tasks'
 import { Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5001/tasks'
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
-  getTask(): Observable<Task[]> {
-    const tasks =  of (TASKS);
-    return tasks
+  getTasks(): Observable<Task[]>{
+return this.http.get<Task[]>(this.apiUrl)
   }
 }
